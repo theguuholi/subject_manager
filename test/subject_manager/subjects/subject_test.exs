192ab2c7e33bf05f7dde7c77fd5890043ebd46ab
Gustavo Oliveira <g.92oliveira@gmail.com>
2026-07-30 09:04:29 -0300

    Fix validation for bio length in changeset and add comprehensive tests for subject

---
+++ test/subject_manager/subjects/subject_test.exs
@@ -0,0 +1,65 @@
+defmodule SubjectManager.Subjects.SubjectTest do
+  use SubjectManager.DataCase, async: true
+
+  alias SubjectManager.Subjects.Subject
+
+  describe "changeset/2" do
+    test "given valid attributes when changeset is called then returns a valid changeset" do
+      attrs = %{
+        name: "Lionel Messi",
+        team: "Inter Miami",
+        position: :forward,
+        bio: "Argentine forward",
+        image_path: "/images/lionel-messi.jpg"
+      }
+
+      changeset = Subject.changeset(%Subject{}, attrs)
+
+      assert changeset.valid?
+      assert changeset.changes == attrs
+    end
+
+    test "given missing required attributes when changeset is called then returns required field errors" do
+      changeset = Subject.changeset(%Subject{}, %{})
+
+      refute changeset.valid?
+
+      assert errors_on(changeset) == %{
+               name: ["can't be blank"],
+               team: ["can't be blank"],
+               position: ["can't be blank"],
+               bio: ["can't be blank"]
+             }
+    end
+
+    test "given a name shorter than three characters when changeset is called then returns a name length error" do
+      attrs = %{
+        name: "Jo",
+        team: "Inter Miami",
+        position: :forward,
+        bio: "Argentine forward",
+        image_path: "/images/jo.jpg"
+      }
+
+      changeset = Subject.changeset(%Subject{}, attrs)
+
+      refute changeset.valid?
+      assert errors_on(changeset).name == ["should be at least 3 character(s)"]
+    end
+
+    test "given an unsupported position when changeset is called then returns a position error" do
+      attrs = %{
+        name: "Lionel Messi",
+        team: "Inter Miami",
+        position: :coach,
+        bio: "Argentine forward",
+        image_path: "/images/lionel-messi.jpg"
+      }
+
+      changeset = Subject.changeset(%Subject{}, attrs)
+
+      refute changeset.valid?
+      assert {"is invalid", _options} = changeset.errors[:position]
+    end
+  end
+end
