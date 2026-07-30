e279bfcde3871f21a1ac3a0af24e11baf4bb27e4
Gustavo Oliveira <g.92oliveira@gmail.com>
2026-07-30 09:46:29 -0300

    Add subject component and filter form to enhance subject listing functionality

--- lib/subject_manager_web/live/subject_live/index.ex
+++ lib/subject_manager_web/live/subject_live/index.ex
@@ -1,7 +1,7 @@
 defmodule SubjectManagerWeb.SubjectLive.Index do
   use SubjectManagerWeb, :live_view
 
-  import SubjectManagerWeb.CustomComponents
+  import SubjectManagerWeb.SubjectLive.Component
 
   alias SubjectManager.Subjects
 
@@ -50,62 +50,6 @@ defmodule SubjectManagerWeb.SubjectLive.Index do
      |> stream(:subjects, new_subjects)
      |> assign(:offset, socket.assigns.offset + length(new_subjects))
      |> assign(:has_more, length(new_subjects) == @page_size)}
-  end
-
-  attr(:subject, SubjectManager.Subjects.Subject, required: true)
-  attr(:dom_id, :string, required: true)
-
-  def subject(assigns) do
-    ~H"""
-    <.link navigate="#" id={@dom_id}>
-      <div class="card">
-        <img src={@subject.image_path} />
-        <h2>{@subject.name}</h2>
-        <div class="details">
-          <div class="team">
-            {@subject.team} - "~p/subjects/@subject"
-          </div>
-          <.badge status={@subject.position} />
-        </div>
-      </div>
-    </.link>
-    """
-  end
-
-  attr(:form, Phoenix.HTML.Form, required: true)
-
-  def filter_form(assigns) do
-    ~H"""
-    <.form for={@form} id="filter-form" phx-change="filter" phx-submit="filter">
-      <.input field={@form[:q]} placeholder="Search..." autocomplete="off" />
-      <.input
-        type="select"
-        field={@form[:position]}
-        prompt="Position"
-        options={[
-          Forward: "forward",
-          Midfielder: "midfielder",
-          Winger: "winger",
-          Defender: "defender",
-          Goalkeeper: "goalkeeper"
-        ]}
-      />
-      <.input
-        type="select"
-        field={@form[:sort_by]}
-        prompt="Sort By"
-        options={[
-          Name: "name",
-          Team: "team",
-          Position: "position"
-        ]}
-      />
-
-      <.link patch={~p"/subjects"}>
-        Reset
-      </.link>
-    </.form>
-    """
   end
 
   defp filters_from_params(params) do
